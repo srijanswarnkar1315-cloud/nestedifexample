8a49888f9e80eff00af557b918ba2ff85e41de84
srijanswarnkar1315-cloud <srijanswarnkar1315@gmail.com>
2025-10-05 22:39:14 +0530

    Add files via upload

---
+++ nestedifexample.java
@@ -0,0 +1,25 @@
+public class nestedifexample {
+    public static void main(String[]args){
+        int marks=85;
+        if(marks>=50){
+            System.out.println("You passed the exam");
+            if(marks>=90){
+                System.out.println("Grade:A+");
+
+            }else if(marks>=75){
+                System.out.println("Grade:A");
+
+
+            }else{
+                System.out.println("Grade:B");
+
+            }
+
+            
+
+        }else{
+            System.out.println("You failed the exam");
+        }
+    }
+    
+}
